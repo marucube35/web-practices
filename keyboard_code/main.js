a63c8e819e3b60bb7e6415032365f1fa1a552fe0
Lê Minh Quân <marucube35@gmail.com>
2022-06-26 12:15:51 +0700

    keyboard_code: done

--- keyboard_code/main.js
+++ keyboard_code/main.js
@@ -1,7 +1,23 @@
 const body = document.querySelector('body')
-const html = document.querySelector('html')
+const wrapper = document.querySelector('.wrapper')
+// ---- message ----
+const message = document.querySelector('.message')
+message.innerHTML = 'Press any keyboard key <span class="key-name"></span>'
 
-body.addEventListener('keypress', (e)=>{
-    console.log(e.key)
-    console.log(e.which)
+body.addEventListener('keypress', (e) => {
+    let key = e.key
+    let code = e.which
+    if (code === 32) key = 'Backspace'
+
+    const keyName = document.createElement('span')
+    keyName.classList.add('key-name')
+    keyName.innerText = key
+    message.innerHTML = `You pressed `
+    message.appendChild(keyName)
+
+    const keyCode = document.createElement('div')
+    keyCode.classList.add('key-code')
+    keyCode.innerText = `${code}`
+    wrapper.removeChild(wrapper.lastChild)
+    wrapper.appendChild(keyCode)
 })
