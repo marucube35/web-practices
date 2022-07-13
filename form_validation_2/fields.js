4c2ce632cc3356f6c7ed016e83fb2553b23ff8a6
Lê Minh Quân <marucube35@gmail.com>
2022-07-13 22:16:24 +0700

    validation_2: remake project

--- form_validation_2/fields.js
+++ form_validation_2/fields.js
@@ -1,26 +1,26 @@
-export const fields = [
-    {
+export const fields = {
+    'full-name': {
         name: 'Full name',
         placeholder: 'Example: Camille Ferros',
         pattern: /^[a-zA-z]+$/gi,
         error: 'Full name only contains character'
     },
-    {
+    email: {
         name: 'Email',
         placeholder: 'Example: email@domain.com',
         pattern: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/gi,
         error: 'Email is invalid'
     },
-    {
+    password: {
         name: 'Password',
         placeholder: 'Enter password',
         pattern: /.{6,}/g,
         error: "Password's length must be more than six characters"
     },
-    {
+    'confirm-password': {
         name: 'Confirm password',
         placeholder: 'Confirm password',
         pattern: /^[a-zA-Z0-9@_-]+$/gi,
         error: 'Password must be alphanumeric (@, _ and - are allowed)'
     }
-]
+}
--- form_validation_2/validator.js
+++ form_validation_2/validator.js
@@ -3,109 +3,167 @@ import { fields } from './fields.js'
 const $ = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
 
+const emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/gi
+
 const wrapper = $('.wrapper')
 const title = $('.form-title')
 const subtitle = $('.form-subtitle')
 const button = $('.form-button')
 
 class Validator {
-    static isFullFilled = function (element) {
-        if (element.value.length === 0)
-            return `Please enter your ${element.id.replace('-', ' ')}`
-        return undefined
+    static isFullFilled(selector, message) {
+        return {
+            check: function (configs) {
+                const element = $(selector)
+                const expression = element.value.length !== 0
+
+                if (expression) return undefined
+                return message
+            }
+        }
     }
-    static isValid = function (element, fields) {
-        const formGroupElement = element.closest('.form-group')
-        const field = fields[formGroupElement.dataset.index]
+    static isEmail(selector, message) {
+        return {
+            check: function (configs) {
+                const element = $(selector)
+                const expression = element.value.match(emailPattern)
 
-        const pattern = field.pattern
-        const error = field.error
-
-        if (!!element.value.match(pattern) || element.value.length === 0)
-            return undefined
-        return error
+                if (expression) return undefined
+                return message
+            }
+        }
     }
-    static isMatched = function (element) {
-        const passwordElement = $(app.passwordSelector)
-        const password = passwordElement.value
+    static minLength(selector, message, min) {
+        return {
+            check: function (configs) {
+                const element = $(selector)
+                const expression = element.value.length >= min
 
-        if (password !== element.value) return 'Passwords are not matched'
-        return undefined
+                if (expression) return undefined
+                return message
+            }
+        }
+    }
+    static isMatched(selector, message) {
+        return {
+            check: function (configs) {
+                const element = $(selector)
+                const form = $(configs.formSelector)
+                const passwordInput = form.querySelector(configs.passwordSelector)
+                const expression = element.value === passwordInput.value
+
+                if (expression) return undefined
+                return message
+            }
+        }
     }
 }
 
 const app = {
-    configurate: function (configurations) {
-        let { formSelector, ...rest } = configurations
-        this.currentForm = $(formSelector)
-        Object.assign(this, rest)
+    configurate: function (options) {
+        Object.assign(this, options)
+
+        this.configs = {
+            formSelector: this.formSelector,
+            passwordSelector: this.passwordSelector
+        }
     },
+
     renderForm: function () {
-        const fieldElements = this.fields.map((field, index) => {
-            return `
-            <div class="form-group" data-index="${index}">
-                <label class="form-label" for="form-input">${field.name}</label>
-                <input class="form-input" 
-                id="${field.name
-                    .replace(' ', '-')
-                    .toLowerCase()}" placeholder="${field.placeholder}" 
-                type="${field.name.match(/password/gi) ? 'password' : ''}"
-                ></input>
+        for (const fieldID in this.fields) {
+            const field = this.fields[fieldID]
+
+            const formGroupElement = `
+            <div class="form-group">
+                <label class="form-label" for="${fieldID}-input">${
+                field.name
+            }</label>
+                <input class="form-input"
+                id="${fieldID}-input" placeholder="${field.placeholder}"
+                type="${field.name.match(/password/gi) ? '' : ''}">
+                </input>
                 <p class="form-message"></p>
             </div>
             `
-        })
-        this.currentForm.innerHTML = fieldElements.join('')
+            $(this.formSelector).innerHTML += formGroupElement
+        }
     },
+
+    validateForm: function (element, message) {
+        const formGroup = element.parentElement
+        const messageElement = formGroup.querySelector('.form-message')
+
+        if (!message) {
+            messageElement.innerText = ''
+            formGroup.classList.remove('form--invalid')
+        } else {
+            messageElement.innerText = message
+            formGroup.classList.add('form--invalid')
+        }
+    },
+
     handleEvents: function () {
         this.rules.forEach((rule) => {
             const element = $(rule.selector)
-            const test = rule.test
-
             element.onblur = function () {
-                app.validate(element, test)
+                const validator = rule.validator
+                const message = validator.check(app.configs)
+
+                app.validateForm(element, message)
             }
 
             element.oninput = function () {
-                const parent = element.parentElement
-                const messageElement = parent.querySelector(app.errorSelector)
+                const formGroup = element.parentElement
+                const messageElement = formGroup.querySelector('.form-message')
 
                 messageElement.innerText = ''
-                parent.classList.remove(app.invalidClass)
+                formGroup.classList.remove('form--invalid')
             }
         })
     },
-    validate: function (element, test) {
-        const parent = element.parentElement
-        const messageElement = parent.querySelector(this.errorSelector)
-        const error = test(element, app.fields)
 
-        if (error) {
-            messageElement.innerText = error
-            parent.classList.add(this.invalidClass)
-        } else {
-            messageElement.innerText = ''
-            parent.classList.remove(this.invalidClass)
-        }
-    },
     start: function () {
         const formOne = {
             formSelector: '#form-1',
-            errorSelector: '.form-message',
-            passwordSelector: '#password',
-            invalidClass: 'form--invalid',
+            passwordSelector: '#password-input',
             fields,
             rules: [
-                { selector: '#full-name', test: Validator.isFullFilled },
-                { selector: '#email', test: Validator.isValid },
-                { selector: '#password', test: Validator.isValid },
-                { selector: '#confirm-password', test: Validator.isMatched }
+                {
+                    selector: '#full-name-input',
+                    validator: Validator.isFullFilled(
+                        '#full-name-input',
+                        'This field can not be empty'
+                    )
+                },
+                {
+                    selector: '#email-input',
+                    validator: Validator.isEmail(
+                        '#email-input',
+                        'Email is invalid'
+                    )
+                },
+                {
+                    selector: '#password-input',
+                    validator: Validator.minLength(
+                        '#password-input',
+                        'Password must be more than six characters',
+                        6
+                    )
+                },
+                {
+                    selector: '#confirm-password-input',
+                    validator: Validator.isMatched(
+                        '#confirm-password-input',
+                        'Passwords are not matched'
+                    )
+                }
             ]
         }
 
         this.configurate(formOne)
         this.renderForm()
         this.handleEvents()
+        console.log(app)
     }
 }
 
