a85c7ceebc83aea8cc3e8aa55c5b37fc9e489e1e
Lê Minh Quân <marucube35@gmail.com>
2022-07-13 13:29:18 +0700

    validation_2: finish html and styles

---
+++ form_validation_2/fields.js
@@ -0,0 +1,26 @@
+export const fields = [
+    {
+        name: 'Full name',
+        placeholder: 'Example: Camille Ferros',
+        pattern: '',
+        error: ''
+    },
+    {
+        name: 'Email',
+        placeholder: 'Example: email@domain.com',
+        pattern: '',
+        error: ''
+    },
+    {
+        name: 'Password',
+        placeholder: 'Enter password',
+        pattern: '',
+        error: ''
+    },
+    {
+        name: 'Reenter password',
+        placeholder: 'Reenter password',
+        pattern: '',
+        error: ''
+    }
+]
